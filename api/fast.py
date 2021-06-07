4b9a545c36e1b73ddfd9dfc5032de7b69cf85a0a
Andreas Mussger <andreas.mussger@gmail.com>
2021-06-07 15:38:14 +0200

    fast API shell added

--- api/fast.py
+++ api/fast.py
@@ -1,1 +1,40 @@
-#TBD
+from datetime import datetime
+import pytz
+
+import pandas as pd
+import joblib
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@app.get("/")
+def index():
+    return dict(greeting="hello")
+
+
+@app.get("/predict")
+def predict(
+        X,  # [28,28]
+        ):  # 1
+
+    # ⚠️ TODO: get model from GCP
+
+    # pipeline = get_model_from_gcp()
+    pipeline = joblib.load('model.joblib')
+
+    # make prediction
+    results = pipeline.predict(X)
+
+
+    return results
